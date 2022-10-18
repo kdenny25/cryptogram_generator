60736c3400ec5fd4d8dde4083627f0eb2c590ba4
kdenny25 <kelsead25@gmail.com>
2022-10-18 08:05:39 -0400

    Created a GUI. You can load the file and create the cryptograms. There is minimal feedback at the moment.

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
 import pandas as pd
 import xlsxwriter
 import tkinter as tk
+import customtkinter as ctk
 import os
 from tkinter import filedialog
 from backend.core.encrypt_phrase import encryptPhrase
 from backend.core.file_management import importFile
 
+ctk.set_appearance_mode("System")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
+app = ctk.CTk()  # create CTk window like you do with the Tk window
 
-root = tk.Tk()
-root.withdraw()
+# window setup
+app.geometry('600x340')
+app.title('Cryptogram Generator')
 
-file_path = filedialog.askopenfilename()
+# create 3x3 grid system
+app.grid_rowconfigure((2,0), weight=1)
+app.grid_columnconfigure((0,2), weight=1)
+
+def load_file(file_path, textbox):
+    file_path = filedialog.askopenfilename()
+    textbox.insert('0.0',"File Path: " + file_path)
+    return file_path
+
+def generate_cryptograms(file_path):
+    if file_path != '':
+        final_df = createCryptograms(file_path, 3)
+        save_path = os.path.splitext(file_path)
+        save_path = save_path[0] + "test_encrypt.xlsx"
+        print(save_path)
+        final_df.to_excel(save_path, engine='xlsxwriter')
 
 def createCryptograms(file_path, num_hints):
+    """ returns dataframe of original file with encrypted phrases
+        and hints. """
     # import file to generate cryptograms
     phrases = importFile(file_path)
     crypto_gen = encryptPhrase()
@@ -38,9 +60,22 @@ def createCryptograms(file_path, num_hints):
 
     return df_copy
 
-if file_path != '':
-    final_df = createCryptograms(file_path, 3)
-    save_path = os.path.splitext(file_path)
-    save_path = save_path[0] + "test_encrypt.xlsx"
-    print(save_path)
-    final_df.to_excel(save_path, engine='xlsxwriter')
+file_path = ''
+
+results_text_box = ctk.CTkTextbox()
+results_text_box.grid(row=0, column=0, columnspan=3, padx=20, pady=(20,0), sticky='nsew')
+results_text_box.insert('0.0','Select File.')
+
+# Button to open file dialogue and select the file to use.
+button_load_file = ctk.CTkButton(master=app, text='Load File', command=lambda: load_file(file_path, results_text_box))
+button_load_file.grid(row=1, column=0, padx=20, pady= (20,0), sticky='ew')
+
+# Button to run the program and generate encrypted phrases.
+button_create_cryptograms = ctk.CTkButton(master=app, text='Generate Cryptograms',
+                                          command=lambda: generate_cryptograms(file_path))
+button_create_cryptograms.grid(row=1, column=2, padx=20, pady=(20,0), sticky='ew')
+
+label_error = ctk.CTkLabel(master=app, text='Error Messages Here')
+label_error.grid(row=2, column=0, columnspan=3, padx=20, sticky='w')
+
+app.mainloop()
---
+++ readme.md
@@ -0,0 +1,22 @@
+![PyPI - License](https://img.shields.io/github/license/kdenny25/cryptogram_generator)
+
+# Cryptogram Generator
+
+Disclaimer: This is an imperfect app but it gets the job done. 
+
+Selecting an excel spreadsheet with a list of phrase will return
+a new spreasheet with a column of encrypted phrases along with hints.
+
+10/17/2022 - At this time the features are all hardcoded. It only accepts
+.xlsx files and produces a set (3) amount of hints. The UI is only limited
+to a file selection box. 
+
+Future Features Include:
+
+* Improved UI using a customer version of Tkinter - https://github.com/TomSchimansky/CustomTkinter
+* Customize number of hints
+* Selecting how hints are produced. (Frequency, Random)
+* Ability to save the file rather than automatically producing it.
+* Support for multiple file formats i.e. (.csv and maybe .docx)
+* Visual of encrypted files in UI
+
